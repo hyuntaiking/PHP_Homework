3d4e1b4995edc99a1bfc91a9c984c603bb4aa746
hyuntaiking <hyuntaiking@gmail.com>
2017-05-23 21:24:56 +0800

    Add files via upload

--- PrimeNumber.php
+++ PrimeNumber.php
@@ -1,4 +1,5 @@
 <?php
+// 可動態調整最大數($maxNumber)和行數($col)
 $maxNum = 100;
 $col = 10;
 echo '<table border="1" width="100%">';
@@ -13,12 +14,31 @@ for ($i=1; $i<=$maxNum; $i++) {
     if ($i % $col == 0) echo '</tr>';
 }
 echo '</table>';
-
+/*----------------------------------------------------------------------------*/
+/* 程式名稱: isPrimeNumber()                                                   */
+/* 程式說明: 判斷是否為質數                                                      */
+/* 傳回參數: true、false                                                       */
+/* 使用說明:                                                                   */
+/*----------------------------------------------------------------------------*/
 function isPrimeNumber($number) {
-    if ($number%2) {
-        return true;
-    } else {
+    // <1非質數
+    if ($number <= 1) {
         return false;
     }
+    // 排除2、3的倍數，可少4/6的數字判斷
+    if ($number > 2 && $number%2==0) {
+        return false;
+    }
+    if ($number > 3 && $number%3==0) {
+        return false;
+    }
+    $result = true;
+    for ($i = 2; $i <= sqrt($number); $i++) {
+        if ($number % $i == 0) {
+            $result = false;
+            break;
+        }
+    }
+    return $result;
 }
 
